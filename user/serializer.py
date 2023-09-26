f2ab765a55fb7c0d3b161c38d698351a23633dbd
Ammen1 <amenguda@gmail.com>
2023-09-26 19:59:46 +0300

    views and urls for registeration and login

--- user/serializer.py
+++ user/serializer.py
@@ -1,4 +1,4 @@
-from api.models import User
+from user.models import User
 from django.contrib.auth.password_validation import validate_password
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework import serializers
--- user/urls.py
+++ user/urls.py
@@ -6,9 +6,9 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    # path('token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    # path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    # path('register/', views.RegisterView.as_view(), name='auth_register'),
-    # path('test/', views.testEndPoint, name='test'),
-    # path('', views.getRoutes),
+    path('token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', views.RegisterView.as_view(), name='auth_register'),
+    path('test/', views.testEndPoint, name='test'),
+    path('', views.getRoutes),
 ]
