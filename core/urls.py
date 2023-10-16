07e6e1c7bbadd3bbef0f0df33218d8b252bcef48
Ammen1 <amenguda@gmail.com>
2023-10-16 20:29:17 +0300

    urls update

--- core/urls.py
+++ core/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/', include('blog.urls')),
-    path('user/', include('user.urls'))
+    path('api/user/', include('user.urls'))
 ]
 
 if settings.DEBUG:
--- user/views.py
+++ user/views.py
@@ -17,7 +17,7 @@ class MyTokenObtainPairView(TokenObtainPairView):
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
-    permission_classes = (AllowAny,)
+    # permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
 
 
